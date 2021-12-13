60f4eb72b4ae28c7703973eeff6ae33dd55848f5
Jan Mytnik <janmytnik78@gmail.com>
2021-12-13 08:42:46 +0100

    agregamos el main

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "jsdnndsojf"<<endl;
+    cout << "dfjojsodfjjsd"<<endl;
+    cout << "sfisodfhohisdf"<<endl;
+    cout << "sdfhohfsd"<<endl;
+    cout << "osdhdos"<<endl;
+    cout << "jsddsds"<<endl;
+    cout << "jofdsjfsdjfds"<<endl;
+    cout << "ojhosfdsdfdsfh"<<endl;
+    cout << "jfdfdsdsf"<<endl;
+
+    return 0;
+}
